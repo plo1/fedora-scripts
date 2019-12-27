3081e96939c6559b8c63fc40df0d653b37fabd79
plo1 <plo1@terpmail.umd.edu>
2019-12-27 03:06:39 -0500

    added specialized matlab shortcut for prime render offload

--- icon/icon.py
+++ icon/icon.py
@@ -7,51 +7,69 @@ home = os.getenv('HOME')
 icons = {}
 icon_dir = home + '/.local/share/applications/'
 
-def init_icon():
-    eclipse = '[Desktop Entry]\n' \
-        'Name=Eclipse\n' \
-        'Type=Application\n' \
-        'Exec=' + home + '/eclipse/java-latest-released/eclipse/eclipse\n' \
-        'Icon=' + home + '/eclipse/java-latest-released/eclipse/icon.xpm\n' \
-        'Terminal=false\n' \
-        'Comment=Integrated Development Environment\n' \
-        'NoDisplay=false\n' \
-        'Categories=Development;IDE;\n' \
-        'Name[en]=Eclipse\n'
+eclipse = '[Desktop Entry]\n' \
+    'Name=Eclipse\n' \
+    'Type=Application\n' \
+    'Exec=' + home + '/eclipse/java-latest-released/eclipse/eclipse\n' \
+    'Icon=' + home + '/eclipse/java-latest-released/eclipse/icon.xpm\n' \
+    'Terminal=false\n' \
+    'Comment=Integrated Development Environment\n' \
+    'NoDisplay=false\n' \
+    'Categories=Development;IDE;\n' \
+    'Name[en]=Eclipse\n'
 
-    android_studio = '[Desktop Entry]\n' \
-        'Type=Application\n' \
-        'Name=Android Studio\n' \
-        'Icon=' + home + '/android-studio/bin/studio.png\n' \
-        'Exec=' + home + '/android-studio/bin/studio.sh\n' \
-        'Terminal=false\n' \
-        'Categories=Development;IDE;\n'
+android_studio = '[Desktop Entry]\n' \
+    'Type=Application\n' \
+    'Name=Android Studio\n' \
+    'Icon=' + home + '/android-studio/bin/studio.png\n' \
+    'Exec=' + home + '/android-studio/bin/studio.sh\n' \
+    'Terminal=false\n' \
+    'Categories=Development;IDE;\n'
 
-    matlab = '[Desktop Entry]\n' \
-        'Name=Matlab\n' \
-        'Type=Application\n' \
-        'Exec=' + home + '/MATLAB/R2019b/bin/matlab -desktop\n' \
-        'Icon=' + home + '/MATLAB/R2019b/bin/glnxa64/cef_resources/matlab_icon.png\n' \
-        'Terminal=False\n' \
-        'Comment=Computational Programming Environment\n' \
-        'Version=R2019b\n' \
-        'Categories=Development;IDE;\n'
+matlab = '[Desktop Entry]\n' \
+    'Name=Matlab\n' \
+    'Type=Application\n' \
+    'Exec=' + '\"' + home + '/MATLAB/R2019b/bin/matlab\" -desktop -r \"startup(%f)\"\n' \
+    'Icon=' + home + '/MATLAB/R2019b/bin/glnxa64/cef_resources/matlab_icon.png\n' \
+    'Terminal=False\n' \
+    'Comment=Computational Programming Environment\n' \
+    'Version=R2019b\n' \
+    'Categories=Development;IDE;\n'
 
+matlab_nvidia = '[Desktop Entry]\n' \
+    'Name=Matlab\n' \
+    'Type=Application\n' \
+    'Exec=env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia ' \
+        '\"' + home + '/MATLAB/R2019b/bin/matlab\" -desktop -nosoftwareopengl -r \"startup(%f)\"\n' \
+    'Icon=' + home + '/MATLAB/R2019b/bin/glnxa64/cef_resources/matlab_icon.png\n' \
+    'Terminal=False\n' \
+    'Comment=Computational Programming Environment\n' \
+    'Version=R2019b\n' \
+    'Categories=Development;IDE;\n' \
+
+
+def init_icons():
     icons['eclipse.desktop'] = eclipse
     icons['android-studio.desktop'] = android_studio 
     icons['matlab.desktop'] = matlab
-    
+
+
 def create_icon_single():
     for key, value in icons.items():
-        if input(value + '\nCreate icon ' + key + ' ? [y/n]: ') == 'y':
+        if input(value + '\nCreate ' + key + '? [y/n]: ') == 'y':
+            if key == 'matlab.desktop' and input('Use prime render offload? [y/n]: ') == 'y':
+                value = matlab_nvidia
             with open(icon_dir + key , 'w+') as f:
                 f.write(value)
 
 
 def create_icon_batch():
     for key, value in icons.items():
+        if key == 'matlab.desktop' and input('Use prime render offload? [y/n]: ') == 'y':
+            value = matlab_nvidia
         with open(icon_dir + key, 'w+') as f:
             f.write(value)
+
 
 def main():
     if os.geteuid() == 0:
@@ -62,7 +80,7 @@ def main():
         raise Exception('Too many args')
 
     if input('Icon directory:\n\t' + icon_dir + '\nIs this correct? [y/n]: ') == 'y':
-        init_icon()
+        init_icons()
     else:
         return
 
@@ -70,7 +88,7 @@ def main():
         create_icon_batch()
     else:
         create_icon_single()
-        
-    
+
+
 if __name__ == '__main__':
     main()
